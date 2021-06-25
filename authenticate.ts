38f9e7127ee18b468df50e9848dadeb22de02874
Michael Hicks <kustomdesigner@gmail.com>
2021-06-25 16:16:13 -0500

    Finished Auth routes

--- authenticate.ts
+++ authenticate.ts
@@ -6,30 +6,27 @@ import "https://deno.land/x/dotenv/load.ts";
 export async function authenticateUser(ctx: Context, next: any) {
   try {
     const jwt = getCookies(ctx.request);
-    const token: string = jwt.authorization;  
+    const token: string = jwt.authorization || "";  
     const secret:string = Deno.env.get('JWT_SECRET') || "";
 
-    const payload = await verify(token, secret, "HS512"); 
-    
-    if(payload) {
-
-      console.log('middleware ran!');
-      await next();
-
-      // ctx.response.status = 200
-      // ctx.response.body = {
-      //   "message" : "ok"
-      // }
-
-    } else {
-      ctx.response.status = 401
-      ctx.response.body = {
-        "message" : "Unauthorized User"
-      }
+    if(token != "" || token != null || token != undefined) {
+        const payload = await verify(token, secret, "HS512"); 
+        console.log('middleware ran!');
+        await next();
+  
+      } else {
+        ctx.response.status = 401
+        ctx.response.body = {
+          "message" : "Unauthorized User"
+        }
     }
-
+    
   } catch(err) {
-    console.error(err);
+    // console.error(err);
+    ctx.response.status = 401
+    ctx.response.body = {
+      "message" : "Unauthorized User"
+    }
   }
 
 }
